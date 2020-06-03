78095c5d4ec9f176180ec9f9c4c1086b03049572
David Boureau <bdavidxyz@gmail.com>
2020-06-03 17:18:50 +0200

    iteration works

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,17 @@
 class WelcomeController < ApplicationController
 
   def index
-    
+    livres = Livre.all.map do |livre|
+      {
+        titre: livre.titre,
+        auteur: livre.auteur,
+        proprietaire: livre.proprietaire,
+      }
+    end
+    render locals: {
+      livres: livres
+    }
+
   end
     
 end
