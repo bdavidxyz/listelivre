232eedce9f6ffdc411027c8f432afc56f1e19b5b
David Boureau <bdavidxyz@gmail.com>
2020-06-03 15:55:59 +0200

    ok for superuser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  def extract_param(pname)
+    params.extract!(pname).permit(pname).to_h[pname]
+  end
+
 end
--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -5,9 +5,16 @@ class DefaultController < ApplicationController
   end
   
   def post_superadmin
+    password = extract_param("super_mdp")
+    p '----------------------'
+    p password
+    p '----------------------'
+    res = User.create!(email: "db@sel.com", password: password)
     render json: {
       status: "ok"
     }
   end
   
+
+
 end
