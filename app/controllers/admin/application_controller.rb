2f2c35a4a38fda079e3571b1a275588ea2ae5496
David Boureau <bdavidxyz@gmail.com>
2020-06-03 14:55:22 +0200

    added clearance

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,7 +6,11 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    include Clearance::Controller
+    include Clearance::Authentication
+    
     before_action :authenticate_admin
+    before_action :require_login
 
     def authenticate_admin
       # TODO Add authentication logic here.
