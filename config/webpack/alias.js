83beae9463accf23f368d3646c40ea11aae2a2cb
David Boureau <bdavidxyz@gmail.com>
2020-06-05 16:53:54 +0200

    retry es6

---
+++ config/webpack/alias.js
@@ -0,0 +1,8 @@
+module.exports = {
+  resolve: {
+    alias: {
+      "datatables.net": "datatables/media/js/jquery.dataTables.js",
+      "markjs": "mark.js/dist/jquery.mark.js"
+    },
+  },
+} 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+// OVER HERE 🙌 ----- Require the new alias file
+const customConfig = require('./alias')
 
-module.exports = environment
+const nodeModulesLoader = environment.loaders.get('nodeModules')
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null)
+    ? []
+    : [nodeModulesLoader.exclude]
+}
+
+// DOWN HERE ALSO 🙌 ----- Merge new resolve alias to our existing configuration
+environment.config.merge(customConfig)
+
+module.exports = environment  
